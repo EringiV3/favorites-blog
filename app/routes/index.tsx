9322e9f130e1db945dc0959f52086a0c2800d520
Kodai Hagihara <39426616+EringiV3@users.noreply.github.com>
2022-07-08 09:29:22 +0900

    update

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,7 +2,6 @@ export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Welcome to Remix!</h1>
-      <div>hoge</div>
       <ul>
         <li>
           <a
