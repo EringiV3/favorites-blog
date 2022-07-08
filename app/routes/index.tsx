958c11e9ec7492e31a1a6203a6d06f43b041cd45
Kodai Hagihara <39426616+EringiV3@users.noreply.github.com>
2022-07-08 09:44:39 +0900

    update

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix!</h1>
+      <h1>Welcome to eringiv3 favorites!</h1>
       <ul>
         <li>
           <a
